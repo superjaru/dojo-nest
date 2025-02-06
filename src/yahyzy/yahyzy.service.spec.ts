684acfa72fd2ee23975f447a2340656085f01908
unknown <another_jaru@hotmail.com>
2025-02-06 11:35:24 +0700

    update all cases

--- src/yahyzy/yahyzy.service.spec.ts
+++ src/yahyzy/yahyzy.service.spec.ts
@@ -61,7 +61,8 @@ describe('Successul case', () => {
     expect(result).toBe(test);
   });
 
-  it('ทอยได้หน้า [2,1,2,1,2] โดยให้ target มีค่า 3 และผลรวมมีค่า 8', () => {
+  // 3 Kind of card
+  it('(3 Kind of card) ทอยได้หน้า [2,1,2,1,2] โดยให้ target มีค่า 3 และผลรวมมีค่า 8', () => {
     const dice = [2, 1, 2, 1, 2];
     const target = 3;
     const result = 8;
@@ -69,7 +70,9 @@ describe('Successul case', () => {
 
     expect(result).toBe(test);
   });
-  it('ทอยได้หน้า [1,1,2,2,2] โดยให้ target มีค่า 3 และผลรวมมีค่า 8', () => {
+
+  // 3 Kind of card
+  it('(3 Kind of card) ทอยได้หน้า [1,1,2,2,2] โดยให้ target มีค่า 3 และผลรวมมีค่า 8', () => {
     const dice = [1, 1, 2, 2, 2];
     const target = 3;
     const result = 8;
@@ -77,8 +80,8 @@ describe('Successul case', () => {
 
     expect(result).toBe(test);
   });
-  // X Kind of card
-  it('ทอยได้หน้า [4,4,4,4,1] โดยให้ target มีค่า 4 และผลรวมมีค่า 17', () => {
+   // 4 Kind of card
+   it('(4 Kind of card) ทอยได้หน้า [4,4,4,4,1] โดยให้ target มีค่า 4 และผลรวมมีค่า 17', () => {
     const dice = [4, 4, 4, 4, 1];
     const target = 4;
     const result = 17;
@@ -86,8 +89,8 @@ describe('Successul case', () => {
 
     expect(result).toBe(test);
   });
-   // X Kind of card
-  it('ทอยได้หน้า [4,4,1,4,4] โดยให้ target มีค่า 4 และผลรวมมีค่า 17', () => {
+   // 4 Kind of card
+  it('(4 Kind of card) ทอยได้หน้า [4,4,1,4,4] โดยให้ target มีค่า 4 และผลรวมมีค่า 17', () => {
     const dice = [4, 4, 1, 4, 4];
     const target = 4;
     const result = 17;
@@ -95,13 +98,86 @@ describe('Successul case', () => {
 
     expect(result).toBe(test);
   });
-  it('ทอยได้หน้า [2,2,3,3,3] โดยให้ target  และผลรวมมีค่า 13', () => {
+  //chance
+  it('(chance) ทอยได้หน้า [2,2,3,3,3] โดยให้ผลรวมมีค่า 13', () => {
     const dice = [2,2,3,3,3];
     const result = 13;
     const test = service.sumDiceByChance(dice);
 
     expect(result).toBe(test);
   });
+
+  //fullhouse
+  it('(fullhouse) ทอยได้หน้า [2,2,3,3,3] โดยให้ ผลรวมมีค่า 25', () => {
+    const dice = [2,2,3,3,3];
+    const result = 25;
+    const test = service.sumDiceByFullhouse(dice);
+
+    expect(result).toBe(test);
+  });
+  //fullhouse
+  it('(fullhouse) ทอยได้หน้า [2,3,3,3,2] โดยให้ ผลรวมมีค่า 25', () => {
+    const dice = [2,3,3,3,2];
+    const result = 25;
+    const test = service.sumDiceByFullhouse(dice);
+
+    expect(result).toBe(test);
+  });
+  //fullhouse
+  it('(fullhouse) ทอยได้หน้า [3,2,3,2,3] โดยให้ ผลรวมมีค่า 25', () => {
+    const dice = [3,2,3,2,3];
+    const result = 25;
+    const test = service.sumDiceByFullhouse(dice);
+
+    expect(result).toBe(test);
+  });
+  //small straight
+  it('(small straight) ทอยได้หน้า [1,2,3,4,6] โดยให้ target มีค่า small และผลรวมมีค่า 30', () => {
+    const dice = [1,2,3,4,6];
+    const target = 'small';
+    const result = 30;
+    const test = service.sumDiceByStraight(dice, target);
+
+    expect(result).toBe(test);
+  });
+  it('(small straight) ทอยได้หน้า [3,2,4,6,1] โดยให้ target มีค่า small และผลรวมมีค่า 30', () => {
+    const dice = [3,2,4,6,1];
+    const target = 'small';
+    const result = 30;
+    const test = service.sumDiceByStraight(dice, target);
+
+    expect(result).toBe(test);
+  });
+  it('(large straight) ทอยได้หน้า [2,3,4,5,6] โดยให้ target มีค่า small และผลรวมมีค่า 40', () => {
+    const dice = [2,3,4,5,6];
+    const target = 'large';
+    const result = 40;
+    const test = service.sumDiceByStraight(dice, target);
+
+    expect(result).toBe(test);
+  });
+  it('(large straight) ทอยได้หน้า [6,4,3,2,5] โดยให้ target มีค่า small และผลรวมมีค่า 40', () => {
+    const dice = [6,4,3,2,5];
+    const target = 'large';
+    const result = 40;
+    const test = service.sumDiceByStraight(dice, target);
+
+    expect(result).toBe(test);
+  });
+  it('(yahtzy) ทอยได้หน้า [3,3,3,3,3] โดยให้ ผลรวมมีค่า 50', () => {
+    const dice = [3,3,3,3,3];
+    const result = 50;
+    const test = service.sumDiceByYahtzy(dice);
+
+    expect(result).toBe(test);
+  });
+
+
+
+
+
+
+
 });
 
 describe('Alternative case', () => {
@@ -115,7 +191,64 @@ describe('Alternative case', () => {
     service = module.get<YahyzyService>(YahyzyService);
   });
 
-  it('ทอยได้หน้า [2,3,2,5,1] โดยให้ target มีค่า 4 และผลรวมมีค่า 0', () => {
+
+  it('(common validation) ทอยได้หน้า [] โดยให้ target มีค่า 3 แต่ result error คือ "Dice must be provided"', () => {
+    const dice = [];
+    const target = 3;
+    const expected = 'Dice must be provided';
+    const result = () => service.checkError(dice, target);
+
+    expect(result).toThrow(expected);
+  });
+  it('(common validation) ทอยได้หน้า [1,3,4] โดยให้ target มีค่า 1 แต่ result error คือ "Dice array length must be 5 long"', () => {
+    const dice = [1, 3, 4];
+    const target = 1;
+    const expected = 'Dice array length must be 5 long';
+    const result = () => service.checkError(dice, target);
+
+    expect(result).toThrow(expected);
+  });
+  it('(common validation) ทอยได้หน้า [1,3,4,1,1,1] โดยให้ target มีค่า 1 แต่ result error คือ "Dice array length must be 5 long"', () => {
+    const dice = [1, 3, 4, 1, 1, 1];
+    const target = 1;
+    const expected = 'Dice array length must be 5 long';
+    const result = () => service.checkError(dice, target);
+
+    expect(result).toThrow(expected);
+  });
+  it('(common validation) ทอยได้หน้า [2,3,2,5,1] โดยให้ target มีค่า -1 แต่ result error คือ "Target must be between 1-6"', () => {
+    const dice = [2, 3, 2, 5, 1];
+    const target = -1;
+    const expected = 'Target must be between 1-6';
+    const result = () => service.checkError(dice, target);
+
+    expect(result).toThrow(expected);
+  });
+  it('(common validation) ทอยได้หน้า [2,3,2,5,1] โดยให้ target มีค่า 7 แต่ result error คือ "Target must be between 1-6"', () => {
+    const dice = [2, 3, 2, 5, 1];
+    const target = 7;
+    const expected = 'Target must be between 1-6';
+    const result = () => service.checkError(dice, target);
+
+    expect(result).toThrow(expected);
+  });
+  it('(target) ทอยได้หน้า [2,3,2,5,7] โดยให้ target มีค่า 1 แต่ result error คือ "Dice must be between 1-6"', () => {
+    const dice = [2, 3, 2, 5, 7];
+    const target = 1;
+    const expected = 'Dice must be between 1-6';
+    const result = () => service.sumDiceByTarget(dice, target);
+
+    expect(result).toThrow(expected);
+  });
+  it('(target) ทอยได้หน้า [2,3,2,5,-1] โดยให้ target มีค่า 1 แต่ result error คือ "Dice must be between 1-6"', () => {
+    const dice = [2, 3, 2, 5, -1];
+    const target = 1;
+    const expected = 'Dice must be between 1-6';
+    const result = () => service.sumDiceByTarget(dice, target);
+
+    expect(result).toThrow(expected);
+  });
+  it('(target) ทอยได้หน้า [2,3,2,5,1] โดยให้ target มีค่า 4 และผลรวมมีค่า 0', () => {
     const dice = [2, 3, 2, 5, 1];
     const target = 4;
     const expected = 0;
@@ -124,60 +257,114 @@ describe('Alternative case', () => {
     expect(result).toBe(expected);
   });
 
-  it('ทอยได้หน้า [] โดยให้ target มีค่า 3 แต่ result error คือ "Dice must be provided"', () => {
-    const dice = [];
+  it('(3kind of card) ทอยได้หน้า [1,2,3,4,5] โดยให้ target มีค่า 3 และผลรวมมีค่า 0', () => {
+    const dice = [1,2,3,4,5];
     const target = 3;
-    const expected = 'Dice must be provided';
-    const result = () => service.sumDiceByTarget(dice, target);
+    const expected = 0;
+    const result = service.sumDiceByKind(dice, target);
+
+    expect(result).toBe(expected);
+  });
+  it('(3kind of card) ทอยได้หน้า [1,2,3,4,5] โดยให้ target มีค่า 4 และผลรวมมีค่า 0', () => {
+    const dice = [1,2,3,4,5];
+    const target = 4;
+    const expected = 0;
+    const result = service.sumDiceByKind(dice, target);
+
+    expect(result).toBe(expected);
+  });
+  it('(3kind of card) ทอยได้หน้า [2,2,2,1,1] โดยให้ target มีค่า 2 แต่ result error คือ "Target must be 3 or 4"', () => {
+    const dice = [2,2,2,1,1];
+    const target = 2;
+    const expected = "Target must be 3 or 4";
+    const result = () => service.sumDiceByKind(dice, target);
 
     expect(result).toThrow(expected);
   });
-  it('ทอยได้หน้า [1,3,4] โดยให้ target มีค่า 1 แต่ result error คือ "Dice array length must be 5 long"', () => {
-    const dice = [1, 3, 4];
-    const target = 1;
-    const expected = 'Dice array length must be 5 long';
-    const result = () => service.sumDiceByTarget(dice, target);
+  it('(3kind of card) ทอยได้หน้า [2,2,2,2,1] โดยให้ target มีค่า 5 แต่ result error คือ "Target must be 3 or 4"', () => {
+    const dice = [2,2,2,2,1];
+    const target = 5;
+    const expected = "Target must be 3 or 4";
+    const result = () => service.sumDiceByKind(dice, target);
 
     expect(result).toThrow(expected);
   });
-  it('ทอยได้หน้า [1,3,4,1,1,1] โดยให้ target มีค่า 1 แต่ result error คือ "Dice array length must be 5 long"', () => {
-    const dice = [1, 3, 4, 1, 1, 1];
-    const target = 1;
-    const expected = 'Dice array length must be 5 long';
-    const result = () => service.sumDiceByTarget(dice, target);
+  it('(fullhouse) ทอยได้หน้า [2,3,3,3,3] โดยให้ result 0', () => {
+    const dice = [2,3,3,3,3];
+    const expected = 0;
+    const result = service.sumDiceByFullhouse(dice);
 
-    expect(result).toThrow(expected);
+    expect(result).toBe(expected);
   });
-  it('ทอยได้หน้า [2,3,2,5,1] โดยให้ target มีค่า -1 แต่ result error คือ "Target must be between 1-6"', () => {
-    const dice = [2, 3, 2, 5, 1];
-    const target = -1;
-    const expected = 'Target must be between 1-6';
-    const result = () => service.sumDiceByTarget(dice, target);
+  it('(fullhouse) ทอยได้หน้า [3,3,3,3,3] โดยให้ result 0', () => {
+    const dice = [3,3,3,3,3];
+    const expected = 0;
+    const result = service.sumDiceByFullhouse(dice);
 
-    expect(result).toThrow(expected);
+    expect(result).toBe(expected);
   });
-  it('ทอยได้หน้า [2,3,2,5,1] โดยให้ target มีค่า 7 แต่ result error คือ "Target must be between 1-6"', () => {
-    const dice = [2, 3, 2, 5, 1];
-    const target = 7;
-    const expected = 'Target must be between 1-6';
-    const result = () => service.sumDiceByTarget(dice, target);
+  it('(straight) ทอยได้หน้า [2,3,3,2,4] โดยให้ target มีค่า small แต่ result 0', () => {
+    const dice = [2,3,3,2,4];
+    const target = 'small';
+    const expected = 0;
+    const result = service.sumDiceByStraight(dice, target);
 
-    expect(result).toThrow(expected);
+    expect(result).toBe(expected);
   });
-  it('ทอยได้หน้า [2,3,2,5,7] โดยให้ target มีค่า 1 แต่ result error คือ "Dice must be between 1-6"', () => {
-    const dice = [2, 3, 2, 5, 7];
-    const target = 1;
-    const expected = 'Dice must be between 1-6';
-    const result = () => service.sumDiceByTarget(dice, target);
+  it('(straight) ทอยได้หน้า [2,3,3,2,4] โดยให้ target มีค่า large แต่ result 0', () => {
+    const dice = [2,3,3,2,4];
+    const target = 'large';
+    const expected = 0;
+    const result = service.sumDiceByStraight(dice, target);
 
-    expect(result).toThrow(expected);
+    expect(result).toBe(expected);
   });
-  it('ทอยได้หน้า [2,3,2,5,-1] โดยให้ target มีค่า 1 แต่ result error คือ "Dice must be between 1-6"', () => {
-    const dice = [2, 3, 2, 5, -1];
-    const target = 1;
-    const expected = 'Dice must be between 1-6';
-    const result = () => service.sumDiceByTarget(dice, target);
 
-    expect(result).toThrow(expected);
+  it('(yahtzy) ทอยได้หน้า [2,3,3,3,3] โดยให้ ผลรวมมีค่า 0', () => {
+    const dice = [2,3,3,3,3];
+    const result = 0;
+    const test = service.sumDiceByYahtzy(dice);
+
+    expect(result).toBe(test);
   });
+  it('(yahtzy) ทอยได้หน้า [3,3,3,3,1] โดยให้ ผลรวมมีค่า 0', () => {
+    const dice = [3,3,3,3,1];
+    const result = 0;
+    const test = service.sumDiceByYahtzy(dice);
+
+    expect(result).toBe(test);
+  });
+  it('(yahtzy) ทอยได้หน้า [3,3,1,3,3] โดยให้ ผลรวมมีค่า 0', () => {
+    const dice = [3,3,1,3,3];
+    const result = 0;
+    const test = service.sumDiceByYahtzy(dice);
+
+    expect(result).toBe(test);
+  });
+
+  it('(yahtzy) ทอยได้หน้า [3,3,1,3,3] โดยให้ ผลรวมมีค่า 0', () => {
+    const dice = [3,3,1,3,3];
+    const result = 0;
+    const test = service.sumDiceByYahtzy(dice);
+
+    expect(result).toBe(test);
+  });
+
+  it('(yahtzy) ทอยได้หน้า [3,1,3,3,3] โดยให้ ผลรวมมีค่า 0', () => {
+    const dice = [3,3,3,3,1];
+    const result = 0;
+    const test = service.sumDiceByYahtzy(dice);
+
+    expect(result).toBe(test);
+  });
+
+  it('(yahtzy) ทอยได้หน้า [1,3,3,3,3] โดยให้ ผลรวมมีค่า 0', () => {
+    const dice = [1,3,3,3,3];
+    const result = 0;
+    const test = service.sumDiceByYahtzy(dice);
+
+    expect(result).toBe(test);
+  });
+
+
 });
--- src/yahyzy/yahyzy.service.ts
+++ src/yahyzy/yahyzy.service.ts
@@ -14,7 +14,6 @@ export class YahyzyService {
     }
   }
   sumDiceByTarget(dice: number[], target: number): number {
-    this.checkError(dice, target);
     let sum = 0;
     dice.map((v) => {
       if (v < 1 || v > 6) throw new Error('Dice must be between 1-6');
@@ -24,7 +23,6 @@ export class YahyzyService {
   }
 
   sumDiceByKind(dice: number[], target: number): number {
-    this.checkError(dice, target);
     if (target !== 3 && target !== 4) throw new Error('Target must be 3 or 4');
     const counts: { [key: number]: number } = {};
     for (const num of dice) {
@@ -32,7 +30,7 @@ export class YahyzyService {
     }
     for (const key in counts) {
       if (counts[key] >= target) {
-        return dice.reduce((acc, v) => acc + v, 0); // Sum all dice
+        return dice.reduce((acc, v) => acc + v, 0); 
       }
     }
     return 0
@@ -40,5 +38,52 @@ export class YahyzyService {
 
   sumDiceByChance(dice: number[]) {
     return dice.reduce((acc, v) => (acc += v), 0);
+  }
+
+  sumDiceByFullhouse(dice : number[]){
+    const counts: { [key: number]: number } = {};
+    for (const num of dice) {
+      counts[num] = (counts[num] || 0) + 1;
+    }
+
+    let hasThree = false;
+    let hasTwo = false;
+
+    for (const count of Object.values(counts)) {
+      if (count === 3) hasThree = true;
+      if (count === 2) hasTwo = true;
+    }
+
+    return (hasThree && hasTwo) ? 25 : 0
+  }
+  sumDiceByStraight(dice : number[], target : string){
+    let sorted = dice.sort((a,b) => a-b)
+    if (target !== 'small' && target !== 'large') throw new Error("Target must be small or large");
+    let stack = sorted[0];
+    let cnt = 1;
+
+    if (target == 'small'){
+      for  (const num of dice){
+        if (num == stack + 1) {
+          cnt += 1
+          stack = num
+        }
+        if (cnt == 4 ) return 30
+      }
+    }
+    else {
+      for  (const num of dice){
+        if (num == stack + 1) {
+          cnt += 1
+          stack = num
+        }
+        if (cnt == 4 ) return 40
+      }
+    }
+    return 0
+  }
+  sumDiceByYahtzy(dice : number[]) {
+    const unique = [... new Set(dice)]
+    return unique.length === 1 ? 50 : 0
   }
 }
